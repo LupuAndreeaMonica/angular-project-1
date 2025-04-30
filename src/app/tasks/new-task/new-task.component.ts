e0cdd3ee2491752c8d5722328b8356e7aa1dca87
Andreea Lupu <andreea.lupu@scriptics.ro>
2025-04-30 12:18:07 +0300

    Section 1 completed (use services and localstorage)

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {type NewTaskData} from "../task/task.model";
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: 'app-new-task',
@@ -12,8 +13,8 @@ import {type NewTaskData} from "../task/task.model";
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Output() close = new EventEmitter<void>();
+  @Input({required: true}) userId!: string
   // enteredTitle = signal('');
   // enteredSummary = signal('');
   // enteredDate = signal('');
@@ -21,14 +22,17 @@ export class NewTaskComponent {
   enteredSummary = '';
   enteredDate = '';
 
+  private tasksService = inject(TasksService)
+
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    })
+    }, this.userId);
+    this.close.emit();
   }
 }
