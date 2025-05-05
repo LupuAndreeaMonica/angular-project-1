508c6b1130e9d80468bb4732e90d65a78646d333
Andreea Lupu <andreea.lupu@scriptics.ro>
2025-05-05 11:27:39 +0300

    commit on new branch section1_2

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -39,6 +39,7 @@ export class TasksService {
     return this.tasks.filter((task) => task.userId === userId);
   }
   addTask(taskData: NewTaskData, userId: string) {
+    //add tasks
     this.tasks.unshift({
       id: new Date().getTime().toString(),
       summary: taskData.summary,
